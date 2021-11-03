039cec2bbaa189bca3a4baf695bd20247c32661a
Piotr Kalina <piotr.lkalina@gmail.com>
2021-11-03 21:27:13 +0100

    resolve iss_001

--- models/author.go
+++ models/author.go
@@ -7,9 +7,9 @@ import (
 
 type Author struct {
 	gorm.Model
-	FirstName  string `gorm:"column:firstName;index:uq_first_last,unique"`
-	MiddleName string `gorm:"column:middleName"`
-	LastName   string `gorm:"column:lastName;index:uq_first_last,unique"`
-	BirthDate  time.Time
+	FirstName  string    `gorm:"column:firstName;index:uq_first_last,unique"`
+	MiddleName string    `gorm:"column:middleName"`
+	LastName   string    `gorm:"column:lastName;index:uq_first_last,unique"`
+	BirthDate  time.Time `gorm:"column:birthDate"`
 	Books      []Book
 }
