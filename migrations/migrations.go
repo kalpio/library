a919fcfd4c449784c878050dfc228e369f43f7d6
Piotr Kalina <piotr.lkalina@gmail.com>
2022-05-01 23:32:55 +0200

    Added database creator

--- migrations/migrations.go
+++ migrations/migrations.go
@@ -3,18 +3,44 @@ package migrations
 import (
 	"errors"
 	"fmt"
-	"gorm.io/gorm"
 	"library/models"
+
+	"gorm.io/gorm"
 )
 
 var ErrAutoMigration = errors.New("could not auto migrate")
 
+func CreateAndUseDatabase(db *gorm.DB, name string) error {
+	if tx := db.Exec(fmt.Sprintf("CREATE DATABASE %s;", name)); tx.Error != nil {
+		return tx.Error
+	}
+	if tx := db.Exec(fmt.Sprintf("USE %s;", name)); tx.Error != nil {
+		return tx.Error
+	}
+
+	return nil
+}
+
+func DropDatabase(db *gorm.DB, name string) error {
+	sql := fmt.Sprintf(`
+USE master;
+ALTER DATABASE [%s] SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+DROP DATABASE [%s];
+`, name, name)
+
+	if tx := db.Exec(sql); tx.Error != nil {
+		return tx.Error
+	}
+
+	return nil
+}
+
 func UpdateDatabase(db *gorm.DB) error {
 	if err := db.AutoMigrate(&models.Author{}); err != nil {
-		return fmt.Errorf("models: %w", ErrAutoMigration)
+		return fmt.Errorf("models: %w: %v", ErrAutoMigration, err)
 	}
 	if err := db.AutoMigrate(&models.Book{}); err != nil {
-		return fmt.Errorf("book: %w", ErrAutoMigration)
+		return fmt.Errorf("book: %w: %v", ErrAutoMigration, err)
 	}
 
 	return nil
