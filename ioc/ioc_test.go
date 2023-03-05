4544a6a26090527d76ebcde2a7b28be9708b1e18
kalpio <piotr.lkalina@gmail.com>
2023-03-05 21:09:11 +0100

    Refactor ^& ctor for Singleton

--- ioc/ioc_test.go
+++ ioc/ioc_test.go
@@ -1,23 +1,90 @@
 package ioc
 
 import (
+	"library/random"
 	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-type iFakeInterface interface {
-	DoSth()
+type iFirstInterface interface {
+	SetText(s string)
+	GetText() string
+	GetTextFromSecond() string
 }
 
-type iFakeStruct struct {
+type iSecondInterface interface {
+	GetSecondText() string
+	SetSecondText(s string)
 }
 
-func (*iFakeStruct) DoSth() {
+type iFirstStruct struct {
+	Second iSecondInterface
+	text   string
 }
 
-type secondFakeStruct struct {
+func newFirstStruct() *iFirstStruct {
+	return new(iFirstStruct)
+}
+
+func newFirstStructWithSecondInterface(secondInterface iSecondInterface) *iFirstStruct {
+	return &iFirstStruct{Second: secondInterface}
+}
+
+func (f *iFirstStruct) SetText(s string) {
+	f.text = s
+}
+
+func (f *iFirstStruct) GetText() string {
+	return f.text
+}
+
+func (f *iFirstStruct) GetTextFromSecond() string {
+	return f.Second.GetSecondText()
+}
+
+type secondStruct struct {
+	text string
+}
+
+func newSecondStruct() *secondStruct {
+	return new(secondStruct)
+}
+
+func newSecondStructNonPointer() secondStruct {
+	return secondStruct{}
+}
+
+func (second *secondStruct) SetSecondText(s string) {
+	second.text = s
+}
+
+func (second *secondStruct) GetSecondText() string {
+	return second.text
+}
+
+func TestAddTransient(t *testing.T) {
+	t.Run("Add transient not return error",
+		addTransientNotReturnError)
+	t.Run("Add transient not return error and has registered service",
+		addTransientNotReturnErrorAndHasRegisteredService)
+}
+
+func addTransientNotReturnError(t *testing.T) {
+	ass := assert.New(t)
+	clearValues(1)
+	err := AddTransient[iFirstInterface](newFirstStruct)
+	ass.NoError(err)
+}
+
+func addTransientNotReturnErrorAndHasRegisteredService(t *testing.T) {
+	ass := assert.New(t)
+	clearValues(1)
+	err := AddTransient[iFirstInterface](newFirstStruct)
+
+	ass.NoError(err)
+	ass.Len(values, 1)
 }
 
 func TestAddSingleton(t *testing.T) {
@@ -36,7 +103,7 @@ func TestAddSingleton(t *testing.T) {
 func addSingletonNotReturnError(t *testing.T) {
 	ass := assert.New(t)
 	clearValues(1)
-	err := AddSingleton[iFakeInterface](&iFakeStruct{})
+	err := AddSingleton[iFirstInterface](newFirstStruct)
 
 	ass.NoError(err)
 }
@@ -44,7 +111,7 @@ func addSingletonNotReturnError(t *testing.T) {
 func addSingletonNotReturnErrorAndHasRegisteredService(t *testing.T) {
 	ass := assert.New(t)
 	clearValues(1)
-	err := AddSingleton[iFakeInterface](new(iFakeStruct))
+	err := AddSingleton[iFirstInterface](newFirstStruct)
 
 	ass.NoError(err)
 	ass.Len(values, 1)
@@ -54,13 +121,13 @@ func addSingletonFailWhenRegisterSameInterfaceSecondTimes(t *testing.T) {
 	ass := assert.New(t)
 	clearValues(1)
 
-	err := AddSingleton[iFakeInterface](new(iFakeStruct))
+	err := AddSingleton[iFirstInterface](newFirstStruct)
 	ass.NoError(err)
 
-	err = AddSingleton[iFakeInterface](new(iFakeStruct))
+	err = AddSingleton[iFirstInterface](newFirstStruct)
 	ass.Error(err)
 	ass.ErrorIs(err, ErrAlreadyRegistered)
-	var obj iFakeInterface
+	var obj iFirstInterface
 	ass.ErrorContains(err, reflect.TypeOf(&obj).Elem().String())
 }
 
@@ -68,7 +135,7 @@ func addSingletonFailWhenTryingAddTypeWhichNotImplementsInterface(t *testing.T) 
 	ass := assert.New(t)
 	clearValues(1)
 
-	err := AddSingleton[iFakeInterface](&secondFakeStruct{})
+	err := AddSingleton[iFirstInterface](newSecondStruct)
 	ass.ErrorIs(err, ErrInvalidType)
 }
 
@@ -76,55 +143,137 @@ func addSingletonFailWhenTryingAddTypeWhichIsDifferent(t *testing.T) {
 	ass := assert.New(t)
 	clearValues(1)
 
-	err := AddSingleton[iFakeStruct](&secondFakeStruct{})
+	err := AddSingleton[iFirstStruct](newSecondStruct)
 	ass.ErrorIs(err, ErrInvalidType)
 }
 
 func TestGet(t *testing.T) {
-	t.Run("Get return registered instance",
-		getReturnRegisteredInstance)
-	t.Run("Get return registered pointer",
-		getReturnRegisteredPointer)
-	t.Run("Get return registered value",
-		getReturnRegisteredValue)
+	t.Run("Get return registered singleton instance",
+		getReturnRegisteredSingletonInstance)
+	t.Run("Get return not unique registered singleton instance",
+		getReturnNotUniqueRegisteredSingletonInstance)
+	t.Run("Get return registered singleton pointer",
+		getReturnRegisteredSingletonPointer)
+	t.Run("Get return registered singleton value",
+		getReturnRegisteredSingletonValue)
+	t.Run("Get return registered transient instance",
+		getReturnRegisteredTransientInstance)
+	t.Run("Get return unique registered transient instance",
+		getReturnUniqueRegisteredTransientInstance)
+	t.Run("Get return unique registered transient with resolved constructor args",
+		getReturnUniqueRegisteredTransientWithResolvedConstructorArgs)
 }
 
-func getReturnRegisteredInstance(t *testing.T) {
+func getReturnRegisteredSingletonInstance(t *testing.T) {
 	ass := assert.New(t)
 	clearValues(1)
 
-	err := AddSingleton[iFakeInterface](new(iFakeStruct))
+	err := AddSingleton[iFirstInterface](newFirstStruct)
 	ass.NoError(err)
 
-	instance, err := Get[iFakeInterface]()
+	instance, err := Get[iFirstInterface]()
 	ass.NoError(err)
-	ass.IsType(&iFakeStruct{}, instance)
+	ass.IsType(&iFirstStruct{}, instance)
 }
 
-func getReturnRegisteredPointer(t *testing.T) {
+func getReturnNotUniqueRegisteredSingletonInstance(t *testing.T) {
 	ass := assert.New(t)
 	clearValues(1)
 
-	err := AddSingleton[*secondFakeStruct](&secondFakeStruct{})
+	err := AddSingleton[iFirstInterface](newFirstStruct)
 	ass.NoError(err)
 
-	value, err := Get[*secondFakeStruct]()
+	v0, err := Get[iFirstInterface]()
 	ass.NoError(err)
-	ass.IsType(&secondFakeStruct{}, value)
+	ass.IsType(&iFirstStruct{}, v0)
+
+	v0.SetText(random.String(10))
+
+	v1, err := Get[iFirstInterface]()
+	ass.NoError(err)
+
+	ass.Equal(v0.GetText(), v1.GetText())
+	ass.Same(v0, v1)
 }
 
-func getReturnRegisteredValue(t *testing.T) {
+func getReturnRegisteredSingletonPointer(t *testing.T) {
 	ass := assert.New(t)
 	clearValues(1)
 
-	err := AddSingleton[secondFakeStruct](secondFakeStruct{})
+	err := AddSingleton[*secondStruct](newSecondStruct)
 	ass.NoError(err)
 
-	value, err := Get[secondFakeStruct]()
+	value, err := Get[*secondStruct]()
 	ass.NoError(err)
-	ass.IsType(secondFakeStruct{}, value)
+	ass.IsType(&secondStruct{}, value)
+}
+
+func getReturnRegisteredSingletonValue(t *testing.T) {
+	ass := assert.New(t)
+	clearValues(1)
+
+	err := AddSingleton[secondStruct](newSecondStructNonPointer)
+	ass.NoError(err)
+
+	value, err := Get[secondStruct]()
+	ass.NoError(err)
+	ass.IsType(secondStruct{}, value)
+}
+
+func getReturnRegisteredTransientInstance(t *testing.T) {
+	ass := assert.New(t)
+	clearValues(1)
+
+	err := AddTransient[iFirstInterface](newFirstStruct)
+	ass.NoError(err)
+
+	value, err := Get[iFirstInterface]()
+	ass.NoError(err)
+	ass.IsType(new(iFirstStruct), value)
+}
+
+func getReturnUniqueRegisteredTransientInstance(t *testing.T) {
+	ass := assert.New(t)
+	clearValues(2)
+
+	err := AddTransient[iFirstInterface](newFirstStruct)
+	ass.NoError(err)
+
+	v0, err := Get[iFirstInterface]()
+	ass.NoError(err)
+	ass.IsType(&iFirstStruct{}, v0)
+
+	v0.SetText(random.String(10))
+
+	v1, err := Get[iFirstInterface]()
+	ass.NoError(err)
+	ass.IsType(&iFirstStruct{}, v1)
+
+	ass.NotEqual(v0.GetText(), v1.GetText())
+	ass.NotSame(v0, v1)
+}
+
+func getReturnUniqueRegisteredTransientWithResolvedConstructorArgs(t *testing.T) {
+	ass := assert.New(t)
+	clearValues(2)
+
+	err := AddSingleton[iSecondInterface](newSecondStruct)
+	ass.NoError(err)
+
+	second, err := Get[iSecondInterface]()
+	ass.NoError(err)
+	secondText := random.String(10)
+	second.SetSecondText(secondText)
+
+	err = AddTransient[iFirstInterface](newFirstStructWithSecondInterface)
+	ass.NoError(err)
+
+	v0, err := Get[iFirstInterface]()
+	ass.NoError(err)
+
+	ass.Equal(v0.GetTextFromSecond(), secondText)
 }
 
 func clearValues(length int) {
-	values = make(map[reflect.Type]interface{}, length)
+	values = make(map[reflect.Type]*scopeAndInterface, length)
 }
